e826a293fb4ab344420eee705e8994b99378219e
minjeong-kk <minjeongkk0@gmail.com>
2026-07-31 02:06:34 +0900

    Fix: 삭제 파일 복구 #12

--- planet-keeper/src/App.jsx
+++ planet-keeper/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import StartPage from "./Components/Start-Page/StartPage";
+import PlanetCreatePage from "./Components/Planet-Create-Page/PlanetCreatePage";
+import GamePage from "./Components/Game-Page/GamePage";
+import ReportPage from "./Components/Report-Page/ReportPage";
 
 function App() {
-
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/planet-create" element={<PlanetCreatePage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/report" element={<ReportPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   )
 }
 
